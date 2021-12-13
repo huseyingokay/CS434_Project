2f37ca5c59309924b9464c3304591fa20cbc8239
huseyingokay <huseyin.gokay@ozu.edu.tr>
2021-12-13 16:48:00 +0300

    pages are created for admin & student

--- src/com/cs434_project/Main.java
+++ src/com/cs434_project/Main.java
@@ -2,9 +2,9 @@ package com.cs434_project;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,11 +15,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("layout/main.fxml"));
+        stage.setScene(new Scene(root));
         stage.show();
     }
 }
