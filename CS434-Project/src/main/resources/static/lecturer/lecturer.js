b8e275c18a05a5ed018ba6183c9d3a0b96b89f89
muratcankilicci <muratcan.kilicci@ozu.edu.tr>
2022-01-05 18:35:26 +0300

    " changed '

--- CS434-Project/src/main/resources/static/lecturer/lecturer.js
+++ CS434-Project/src/main/resources/static/lecturer/lecturer.js
@@ -13,28 +13,28 @@ function onClickEditExam(){
 
 function onClickWritten(){
     let question = '';
-    question += '<div class="question" id=written' + questionId + ' style='display: inline-grid'>'
-    question += '<p> Question: ' + (questionId + 1) +'</p>'
+    question += '<div class="question" id=written" + questionId + "style="display: inline-grid">'
+    question += '<p> Question: " + (questionId + 1) +"</p>'
     question += '<p>Please add your question explanation: </p>'
-    question += '<textarea id=questionExplanation' + questionId + '> </textarea>'
+    question += '<textarea id=questionExplanation" + questionId + "> </textarea>'
     question += '<p>Please add your answer for this question: </p>'
-    question += '<textarea id=questionAnswer' + questionId + '> </textarea>'
+    question += '<textarea id=questionAnswer" + questionId + "> </textarea>'
     question += '</div>'
     questionId += 1;
-    document.getElementById('createExamMainContext').innerHTML += question;
+    document.getElementById("createExamMainContext").innerHTML += question;
 }
 
 function onClickTrueFalse(){
     let question = '';
-    question += '<div class='question' id=truefalse' + questionId + ' style='display: inline-grid'>'
-    question += '<p> Question: ' + (questionId + 1) +'</p>'
+    question += '<div class="question" id=truefalse" + questionId + " style="display: inline-grid">'
+    question += '<p> Question: " + (questionId + 1) +"</p>'
     question += '<p>Please add your question explanation: </p>'
-    question += '<textarea id=questionExplanation' + questionId + '> </textarea>'
+    question += '<textarea id=questionExplanation" + questionId + "> </textarea>'
     question += '<p>Please add your answer for this question: </p>'
-    question += '<input type='radio' id='html' name='fav_language' value='HTML'>'
-    question += '<label htmlFor='html'>HTML</label><br>'
-    question += '<input type='radio' id='css' name='fav_language' value='CSS'>'
-    question += '<label htmlFor='css'>CSS</label><br>'
+    question += '<input type="radio" id="html" name="fav_language" value="HTML">'
+    question += '<label htmlFor="html">HTML</label><br>'
+    question += '<input type="radio" id="css" name="fav_language" value="CSS">'
+    question += '<label htmlFor="css">CSS</label><br>'
     question += '</div>'
     questionId += 1;
     document.getElementById('createExamMainContext').innerHTML += question;
